f4a315d7812a8c19260382e36b09ee1f8e0ba0da
Abdur Rahman <abdurrahman21266057@gmail.com>
2025-10-21 13:02:39 +0600

    add new file and logic

--- src/homeProductCards.js
+++ src/homeProductCards.js
@@ -1,3 +1,5 @@
+import { homeQuantityToggle } from "./homeQuantityToggle";
+
 const productTemplate = document.querySelector("#productTemplate");
 const productContainer = document.querySelector("#productContainer");
 
@@ -5,54 +7,50 @@ export const showProductContainer = (products) => {
   if (!products) {
     return false;
   }
-  
+
   products.forEach((curProd) => {
-    const { brand, category, description, id, image, name, price, stock ,discount} =
-      curProd;
-
-const  discountedPrice   = discount? price-(price *discount):null
-
+    const {
+      brand,
+      category,
+      description,
+      id,
+      image,
+      name,
+      price,
+      stock,
+      discount,
+    } = curProd;
+    const discountedPrice = discount ? price - (price * discount) / 100 : null;
 
     const productClone = document.importNode(productTemplate.content, true);
     productClone.querySelector(".productName").textContent = name;
-    productClone.querySelector(".productImage").src = image;
-    productClone.querySelector(".productImage").alt  = name;
-    productClone.querySelector('.productStock').textContent = stock;
-    productClone.querySelector('.productDescription').textContent = description;
-        if (discountedPrice ) {
-   
-      productClone.querySelector('.productPrice').textContent = `৳${discountedPrice}`;
-      productClone.querySelector(".productActualPrice").textContent = `₹${price}`;  
+    if (discount) {
+      productClone.querySelector(".category").textContent = `${discount}%`;
     } else {
-   
-      productClone.querySelector('.productPrice').textContent = `৳${price}`;
-      productClone.querySelector('.productActualPrice').style.display = "none"; 
+      productClone.querySelector(".category").style.display = "none";
     }
+    productClone.querySelector(".productImage").src = image;
+    productClone.querySelector(".productImage").alt = name;
+    productClone.querySelector(".productStock").textContent = stock;
+    productClone.querySelector(".productDescription").textContent = description;
 
-productContainer.append(productClone)
+    if (discountedPrice) {
+      productClone.querySelector(
+        ".productPrice"
+      ).textContent = `৳${discountedPrice}`;
+      productClone.querySelector(
+        ".productActualPrice"
+      ).textContent = `${price}`;
+    } else {
+      productClone.querySelector(".productPrice").textContent = `৳${price}`;
+      productClone.querySelector(".productActualPrice").style.display = "none";
+    }
+    productClone.querySelector('.stockElement').addEventListener("clike", (event) =>{
 
+
+      homeQuantityToggle(event,id,stock)
+    })
+
+    productContainer.append(productClone);
   });
 };
-
-
- 
-
-// const productTemplate = document.querySelector("#productTemplate");
-// const productContainer = document.querySelector("#productContainer");
-
-// export const showProductContainer = (products) => {
-//   if (!products) return false;
-
-//   products.forEach((curProd) => {
-//     const { brand, category, description, id, image, name, price, stock } = curProd;
-
-//     // ✅ Correct line
-//     const productClone = document.importNode(productTemplate.content, true);
-
-//     // ✅ Fill data
-//     productClone.querySelector(".productName").textContent = name;
-
-//     // ✅ Append to container
-//     productContainer.append(productClone);
-//   });
-// };
---
+++ src/homeQuantityToggle.js
@@ -0,0 +1,4 @@
+export const homeQuantityToggle = (event,id,stock)=>{
+
+
+}
